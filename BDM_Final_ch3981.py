b14a32b14a8fb0c20beebe2610fb3bb57d420576
toarukagakunoSirius <44242956+toarukagakunoSirius@users.noreply.github.com>
2022-05-17 00:03:27 -0400

    Add files via upload

--- BDM_Final_ch3981.py
+++ BDM_Final_ch3981.py
@@ -14,78 +14,71 @@ def to_datelist(start,end,cbg):
     elif start =='2020-10' or end == '2020-10': return [{},{},{},cbg]
     else: None
 
-def merge_datelist(a,b):
+def merge_datelist(x,y):
     output = [{},{},{},{}]
-    for i in range(len(a)):
-        output[i].update(a[i])
-        output[i].update(b[i])
+    for i in range(len(x)):
+        output[i].update(x[i])
+        output[i].update(y[i])
     return output
 
-def filter_cbg(dict_in,filter_list):
+def filter_cbg(input,centroids):
     output = []
-    for dict_ in dict_in:
-        if dict_ == {}: output.append('')
+    for item in input:
+        if item == {}: output.append('')
         else:
-            dict_out = []
-            for item in dict_:
-                if item in filter_list:
-                    dict_out.append((item,dict_[item]))
-            if dict_out != []:  
-                output.append(dict_out)
-            else:
-                output.append('')
+            out = []
+            for i in item:
+                if i in centroids: out.append((i,item[i]))
+            if out != []: output.append(out)
+            else: output.append('')
     return output
 
-def transform_cbg(input,transfer_list):
+def transform_cbg(input,centroids):
     t = Transformer.from_crs(4326, 2263)
     if type(input) == list: 
-        list_out = []
-        for dict_ in input:
-            if dict_ == '': list_out.append('')
+        output = []
+        for item in input:
+            if item == '': output.append('')
             else:
-                dict_out = []
-                for item1 in dict_:
-                    for item2 in transfer_list:
-                        if item1[0] == item2[0]:
-                            dict_out.append((t.transform(item2[1],item2[2]),item1[1]))
-                list_out.append(dict_out)
-        return list_out
+                out = []
+                for i in item:
+                    for c in centroids:
+                        if i[0] == c[0]: out.append((t.transform(c[1],c[2]),i[1]))
+                output.append(out)
+        return output
     else:
-        for item in transfer_list:
-            if input == item[0]:
-                return t.transform(item[1],item[2])
+        for item in centroids:
+            if input == item[0]: return t.transform(item[1],item[2])
 
-def distance(start_list,destination):
+def distance(start,end):
     output = []
-    for item in start_list:
-        if item == '':
-            output.append('')
+    for item in start:
+        if item == '': output.append('')
         else:
-            distance_list=[]
+            dist=[]
             for start in item:
-                distance_list.append((Point(start[0][0],start[0][1]).distance(Point(destination[0],destination[1]))/5280,start[1]))
-            output.append(distance_list)
+                dist.append((Point(start[0][0],start[0][1]).distance(Point(end[0],end[1]))/5280,start[1]))
+            output.append(dist)
     return output
 
 def mean(input):
     output = []
     for item in input:
-        if item == '':
-            output.append('')
+        if item == '': output.append('')
         else:
-            sum_ = 0
-            num_ = 0
-            for cuple in item:
-                sum_ += cuple[0] * cuple[1]
-                num_ += cuple[1]
-            if num_ != 0:
-                output.append(str(round(sum_/num_,2)))
+            sum = 0
+            count = 0
+            for i in item:
+                sum += i[0] * i[1]
+                count += i[1]
+            if count != 0: output.append(str(round(sum/count,2)))
     return output
 
 if __name__ == "__main__":
     sc = pyspark.SparkContext.getOrCreate()
     spark = SparkSession(sc)
 
+# read
     pattern = sc.textFile('/tmp/bdm/weekly-patterns-nyc-2019-2020').map(lambda x: next(csv.reader([x])))
     header = pattern.first()
     pattern = pattern.filter(lambda row : row != header)
@@ -99,22 +92,24 @@ if __name__ == "__main__":
     header = centroids.first()
     centroids = centroids.filter(lambda row : row != header) 
 
+# extract date
     pat_date = pattern_clean.map( lambda x: (x[3],to_datelist(x[1],x[2],x[4]))).filter(lambda x: x[1] is not None).reduceByKey(lambda x,y: merge_datelist(x,y))
 
+# filter nyc cbg
     centroids_filter = centroids.map(lambda x: x.split(',')[0]).collect()
-
     pat_date_cen = pat_date.map(lambda x: [x[0],filter_cbg(x[1],centroids_filter)])
 
+# transform cbg centroid coordinates
     centroids_list = centroids.map(lambda x: [x.split(',')[0],x.split(',')[1],x.split(',')[2]]).collect()
-
     pat_date_cen = pat_date_cen.map(lambda x: [x[0],transform_cbg(x[0],centroids_list),transform_cbg(x[1],centroids_list)])
 
+# mean distance
     pat_date_dis = pat_date_cen.map(lambda x: [x[0],distance(x[2],x[1])])
-
     pat_date_disM = pat_date_dis.map(lambda x: [x[0],mean(x[1])])
 
-    df_out = pat_date_disM.map(lambda x: [str(x[0]),str(x[1][0]),str(x[1][1]) ,str(x[1][2]),str(x[1][3])])\
-            .toDF(['cbg_fips', '2019-03' , '2019-10' , '2020-03' , '2020-10'])\
+# final output
+    output = pat_date_disM.map(lambda x: [str(x[0]),str(x[1][0]),str(x[1][1]) ,str(x[1][2]),str(x[1][3])])\
+            .toDF(['cbg_fips','2019-03','2019-10','2020-03','2020-10'])\
             .sort('cbg_fips', ascending = True)
 
-    df_out.coalesce(1).write.options(header='true').csv(sys.argv[1])
+    output.coalesce(1).write.options(header='true').csv(sys.argv[1])
