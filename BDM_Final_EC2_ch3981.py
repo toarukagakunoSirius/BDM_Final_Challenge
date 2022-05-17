68b09ec723fcfdd231a2b68f04173b4c4177c217
toarukagakunoSirius <44242956+toarukagakunoSirius@users.noreply.github.com>
2022-05-17 17:49:31 -0400

    Update BDM_Final_EC2_ch3981.py

--- BDM_Final_EC2_ch3981.py
+++ BDM_Final_EC2_ch3981.py
@@ -103,8 +103,6 @@ if __name__ == "__main__":
     header2 = core.first()
     core = core.filter(lambda row : row != header2) 
     filter_list = core.map(lambda x: [x[0], x[9]]).filter(lambda x: x[1].startswith('4451')).collect()
-    #filter_list = filter_list.filter(lambda x: str(x[1]).startswith('4451')).collect()
-    #flst = filter_list.filter(lambda x: x[1] in ['445110','445120']).collect()
     pat = pattern_clean.filter(lambda x: x[0] in filter_list)
 
     cbg = sc.textFile('nyc_cbg_centroids.csv')
