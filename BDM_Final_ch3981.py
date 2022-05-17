93dc59d798d4dd45d7f82f3f6d1876a6cabdd99e
toarukagakunoSirius <44242956+toarukagakunoSirius@users.noreply.github.com>
2022-05-16 22:05:50 -0400

    Add files via upload

--- BDM_Final_ch3981.py
+++ BDM_Final_ch3981.py
@@ -11,17 +11,21 @@ if __name__ == "__main__":
     sc = pyspark.SparkContext.getOrCreate()
     spark = SparkSession(sc)
 
-    def to_datelist(date1,date2,cbgs):
-        if date1 =='2019-03' or date2 == '2019-03':
-            return [cbgs,{},{},{}]
-        elif date1 =='2019-10' or date2 == '2019-10':
-            return [{},cbgs,{},{}]
-        elif date1 =='2020-03' or date2 == '2020-03':
-            return [{},{},cbgs,{}]
-        elif date1 =='2020-10' or date2 == '2020-10':
-            return [{},{},{},cbgs]
-        else:
-            None
+    pattern = sc.textFile('/tmp/bdm/weekly-patterns-nyc-2019-2020').map(lambda x: next(csv.reader([x])))
+    header = pattern.first()
+    pattern = pattern.filter(lambda row : row != header)
+    rdd_task1 = pattern.map(lambda x: [x[0], '-'.join(x[12].split('T')[0].split('-')[:2]), '-'.join(x[13].split('T')[0].split('-')[:2]), x[18], json.loads(x[19])])
+
+    markets = sc.textFile('nyc_supermarkets.csv')
+    filter_list = markets.map(lambda x: x.split(',')[-2]).collect()
+    rdd_task1 = rdd_task1.filter(lambda x: x[0] in filter_list)
+
+    def to_datelist(start,end,cbg):
+        if start =='2019-03' or end == '2019-03': return [cbg,{},{},{}]
+        elif start =='2019-10' or end == '2019-10': return [{},cbg,{},{}]
+        elif start =='2020-03' or end == '2020-03': return [{},{},cbg,{}]
+        elif start =='2020-10' or end == '2020-10': return [{},{},{},cbg]
+        else: None
 
     def merge_datelist(a,b):
         output = [{},{},{},{}]
@@ -29,6 +33,14 @@ if __name__ == "__main__":
             output[i].update(a[i])
             output[i].update(b[i])
         return output
+
+    rdd_task2 = rdd_task1.map( lambda x: (x[3],to_datelist(x[1],x[2],x[4]))).filter(lambda x: x[1] is not None).reduceByKey(lambda x,y: merge_datelist(x,y))
+
+    rdd_cbg = sc.textFile('nyc_cbg_centroids.csv')
+    header2 = rdd_cbg.first()
+    rdd_cbg = rdd_cbg.filter(lambda row : row != header2) 
+    cbg_filter = rdd_cbg.map(lambda x: x.split(',')[0]).collect()
+    rdd_cbg_list = rdd_cbg.map(lambda x: [x.split(',')[0],x.split(',')[1],x.split(',')[2]]).collect()
 
     def filter_cbg(dict_in,filter_list):
         output = []
@@ -44,6 +56,8 @@ if __name__ == "__main__":
                 else:
                     output.append('')
         return output
+
+    rdd_task3 = rdd_task2.map(lambda x: [x[0],filter_cbg(x[1],cbg_filter)])
 
     def transform_cbg(input,transfer_list):
         t = Transformer.from_crs(4326, 2263)
@@ -64,6 +78,8 @@ if __name__ == "__main__":
                 if input == item[0]:
                     return t.transform(item[1],item[2])
 
+    rdd_task4 = rdd_task3.map(lambda x: [x[0],transform_cbg(x[0],rdd_cbg_list),transform_cbg(x[1],rdd_cbg_list)])
+
     def distance(start_list,destination):
         output = []
         for item in start_list:
@@ -75,6 +91,8 @@ if __name__ == "__main__":
                     distance_list.append((Point(start[0][0],start[0][1]).distance(Point(destination[0],destination[1]))/5280,start[1]))
                 output.append(distance_list)
         return output
+
+    rdd_task4 = rdd_task4.map(lambda x: [x[0],distance(x[2],x[1])])
 
     def mean(input):
         output = []
@@ -90,38 +108,6 @@ if __name__ == "__main__":
                 if num_ != 0:
                     output.append(str(round(sum_/num_,2)))
         return output
-
-    pattern = sc.textFile('/tmp/bdm/weekly-patterns-nyc-2019-2020').map(lambda x: next(csv.reader([x])))
-    header = pattern.first()
-    pattern = pattern.filter(lambda row : row != header)
-
-    rdd_filter = sc.textFile('nyc_supermarkets.csv')
-    rdd_task1 = pattern.map(lambda x: [x[0], '-'.join(x[12].split('T')[0].split('-')[:2]), '-'.join(x[13].split('T')[0].split('-')[:2]), x[18], json.loads(x[19])])
-
-    filter_list = rdd_filter.map(lambda x: x.split(',')[-2]).collect()
-    rdd_task1 = rdd_task1.filter(lambda x: x[0] in filter_list)
-
-    rdd_task2 = rdd_task1.map( lambda x: (x[3],to_datelist(x[1],x[2],x[4]))).filter(lambda x: x[1] is not None).reduceByKey(lambda x,y: merge_datelist(x,y))
-
-    rdd_cbg = sc.textFile('nyc_cbg_centroids.csv')
-    header2 = rdd_cbg.first()
-    rdd_cbg = rdd_cbg.filter(lambda row : row != header2) 
-    cbg_filter = rdd_cbg.map(lambda x: x.split(',')[0]).collect()
-
- 
-
-    rdd_task3 = rdd_task2.map(lambda x: [x[0],filter_cbg(x[1],cbg_filter)])
-
-    rdd_cbg_list = rdd_cbg.map(lambda x: [x.split(',')[0],x.split(',')[1],x.split(',')[2]]).collect()
-
-
-
-    rdd_task4 = rdd_task3.map(lambda x: [x[0],transform_cbg(x[0],rdd_cbg_list),transform_cbg(x[1],rdd_cbg_list)])
-
-
-    rdd_task4 = rdd_task4.map(lambda x: [x[0],distance(x[2],x[1])])
-
-
 
     rdd_task5 = rdd_task4.map(lambda x: [x[0],mean(x[1])])
 
